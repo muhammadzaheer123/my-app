80a5571c25f07438b1f59be5205ec637c73ac3c0
muhammadzaheer <zaheerkhan62145@gmail.com>
2024-06-12 08:52:59 -0700

    "Asus to Apple "

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = () => {
     return (
         <div>
             <div className="container">
-                <ProductCompo img={IMAGES.Laptop1} Tittle={"Asus Laptop"} Description={"Super Cool"} />
+                <ProductCompo img={IMAGES.Laptop1} Tittle={"Apple Laptop"} Description={"Super Cool"} />
 
                 <ProductCompo img={IMAGES.Laptop2} Tittle={"Dell Laptop"} Description={"Super Cool"} />
                 <ProductCompo img={IMAGES.Laptop1} Tittle={"Hp Laptop"} Description={"Super Cool"} />
